d975df92e21574a9ba6434f3f0f909c7319c10a3
Avery Pratt <apratt91@gmail.com>
2017-08-15 19:22:56 -0700

    added shooting functionality to space shooter

---
+++ Space Shooter/Assets/Mover.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+    private void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        body.velocity = transform.forward * speed;
+    }
+}
--- Space Shooter/Assets/PlayerController.cs
+++ Space Shooter/Assets/PlayerController.cs
@@ -14,20 +14,37 @@ public class PlayerController : MonoBehaviour
     public float tilt;
     public Boundary boundary;
 
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextFire;
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+    }
+
     void FixedUpdate()
     {
         Rigidbody body = GetComponent<Rigidbody>();
-        float moveHorizontal = Input.GetAxis("Horizontal") * speed;
-        float moveVertical = Input.GetAxis("Vertical") * speed;
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        body.velocity = movement;
+        body.velocity = movement * speed;
 
-        body.position = new Vector3(
+        body.position = new Vector3
+        (
             Mathf.Clamp(body.position.x, boundary.xMin, boundary.xMax),
             0.0f,
             Mathf.Clamp(body.position.z, boundary.zMin, boundary.zMax)
-            );
+        );
 
         body.rotation = Quaternion.Euler(0.0f, 0.0f, body.velocity.x * -tilt);
     }
