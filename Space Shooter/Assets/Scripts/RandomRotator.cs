948c9b62a498858ff562067e40fc18f822a8b451
Avery Pratt <apratt91@gmail.com>
2017-08-27 12:20:38 -0700

    added asteriod game object

---
+++ Space Shooter/Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+    public float tumble;
+
+    private void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        body.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+}
