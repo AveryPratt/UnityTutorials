3aad4dfc209d37c8dfa378ca3404a525c79cf85d
Avery Pratt <apratt91@gmail.com>
2017-09-21 23:41:29 -0700

    added map generator that can make rorshack tests

--- Procedural Caves/Assets/MapGenerator.cs
+++ Procedural Caves/Assets/MapGenerator.cs
@@ -1,16 +1,116 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class MapGenerator : MonoBehaviour {
+public class MapGenerator : MonoBehaviour
+{
+    public int width;
+    public int height;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public string seed;
+    public bool useRandomSeed;
+
+    [Range(0, 100)]
+    public int randomFillPercent;
+
+    int[,] map;
+
+    private void Start()
+    {
+        GenerateMap();
+    }
+
+    private void GenerateMap()
+    {
+        map = new int[width, height];
+        RandomFillMap();
+
+        for (int i = 0; i < 5; i++)
+        {
+            SmoothMap();
+        }
+    }
+
+    private void RandomFillMap()
+    {
+        if (useRandomSeed)
+        {
+            seed = DateTime.UtcNow.Millisecond.ToString();
+        }
+
+        System.Random pseudoRandom = new System.Random(seed.GetHashCode());
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                {
+                    map[x, y] = 1;
+                }
+                map[x, y] = pseudoRandom.Next(0, 100) < randomFillPercent ? 1 : 0;
+            }
+        }
+    }
+
+    private void SmoothMap()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int neighborWallTiles = GetSurroundingWallCount(x, y);
+
+                if (neighborWallTiles > 4)
+                {
+                    map[x, y] = 1;
+                }
+                else if (neighborWallTiles < 4)
+                {
+                    map[x, y] = 0;
+                }
+            }
+        }
+    }
+
+    private int GetSurroundingWallCount(int gridX, int gridY)
+    {
+        int wallCount = 0;
+
+        for (int neighborX = gridX - 1; neighborX <= gridX + 1; neighborX++)
+        {
+            for (int neighborY = gridY - 1; neighborY <= gridY + 1; neighborY++)
+            {
+                if (neighborX >= 0 && neighborX < width && neighborY >= 0 && neighborY < height)
+                {
+                    if (neighborX != gridX || neighborY != gridY)
+                    {
+                        wallCount += map[neighborX, neighborY];
+                    }
+                }
+                else
+                {
+                    wallCount += 1;
+                }
+            }
+        }
+        return wallCount;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (map != null)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
+                    Vector3 pos = new Vector3(-width / 2 + x + .5f, 0, -height / 2 + y + .5f);
+                    Gizmos.DrawCube(pos, Vector3.one);
+                }
+            }
+        }
+    }
 }
